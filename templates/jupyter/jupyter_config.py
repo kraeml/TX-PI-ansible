a4fee397bc82f8ba2f89d8bb1da04ed9c77f77fd
Michael Schaar <kraeml@sesamestreet.franken.de>
2021-05-19 10:03:06 +0200

    Erster Commit

---
+++ templates/jupyter/jupyter_config.py
@@ -0,0 +1,5 @@
+# You can also disable these options for all JupyterLab builds by adding these
+# lines to a Jupyter config file named `jupyter_config.py`:
+
+c.LabBuildApp.minimize = False
+c.LabBuildApp.dev_build = False
